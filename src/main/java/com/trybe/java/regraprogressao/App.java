6f47092e665a9367f0b96bf8b07ac976ed4b272a
Nícolas <nicolasbrga01@gmail.com>
2023-08-21 17:11:29 -0300

    (Feat) Req 01 - Criando método para inserir atividades e seus pesos

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,5 @@
 package com.trybe.java.regraprogressao;
-
+import java.util.Scanner;
 /**
  * App.
  */
@@ -9,5 +9,25 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite a quantidade de atividades para cadastrar:");
+    int quantidadeAtividades = sc.nextInt();
+    sc.nextLine();
+
+    String[] atividadeNome = new String[quantidadeAtividades];
+    int[] atividadePeso = new int[quantidadeAtividades];
+    int pesoTotal = 0;
+    for (int i = 0; i < quantidadeAtividades; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ":");
+      atividadeNome[i] = sc.nextLine();
+      System.out.println("Digite o peso da atividade " + (i + 1) + ":");
+      atividadePeso[i] = sc.nextInt();
+      sc.nextLine();
+
+      pesoTotal += atividadePeso[i];
+    }
+    if (pesoTotal != 100) {
+      System.out.println("Erro: A soma dos pesos não é igual a 100.");
+    }
   }
 }
